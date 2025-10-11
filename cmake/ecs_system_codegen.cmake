f3039d58f460f67f4ebf7bcf9474cddf0090757d
Sebastian <sebastian.borsch1996@gmail.com>
2025-10-11 21:02:28 +0200

    Fixed code-gen dependency issue for builds

--- cmake/ecs_system_codegen.cmake
+++ cmake/ecs_system_codegen.cmake
@@ -1,29 +1,51 @@
-find_package(Python3 COMPONENTS Interpreter REQUIRED)
+cmake_minimum_required(VERSION 3.20)
+include_guard(GLOBAL)
 
-set(ECS_SCAN_DIRS
+# Set variables
+set(CODEGEN_SCRIPT "${CMAKE_SOURCE_DIR}/tools/system_file_generator.py")
+set(CODEGEN_ROOT_DIRS
         ${CMAKE_SOURCE_DIR}/engine/core/systems
         ${CMAKE_SOURCE_DIR}/gameplay
 )
 
-message(STATUS "ECS Scan Dirs: ${ECS_SCAN_DIRS}")
+set(GENERATED_DIR "${CMAKE_SOURCE_DIR}/generated")
+set(GENERATED_BASE_NAME "ECS_Systems")
+set(GENERATED_CPP_FILE "${GENERATED_DIR}/${GENERATED_BASE_NAME}.gen.cpp")
 
-file(GLOB_RECURSE ECS_SCAN_FILES
+
+# Find python package
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+
+# Create target directory
+file(MAKE_DIRECTORY "${GENERATED_DIR}")
+
+# Inputs for rebuild trigger
+message(STATUS "ECS Scan Dirs: ${CODEGEN_ROOT_DIRS}")
+file(GLOB_RECURSE CODEGEN_INPUTS
         CONFIGURE_DEPENDS
-        ${ECS_SCAN_DIRS}/*.hpp
-        ${ECS_SCAN_DIRS}/*.h
-        ${ECS_SCAN_DIRS}/*.cpp
+        ${CODEGEN_ROOT_DIRS}/*.hpp
+        ${CODEGEN_ROOT_DIRS}/*.h
+        ${CODEGEN_ROOT_DIRS}/*.cpp
+        ${CODEGEN_ROOT_DIRS}/*.inl
+)
+set(CODEGEN_ARGS --out "${GENERATED_CPP_FILE}" --roots)
+list(APPEND CODEGEN_ARGS ${CODEGEN_ROOT_DIRS})
+
+message("Code gen args: ${CODEGEN_ARGS}")
+# Trigger code gen scrip
+add_custom_command(
+        OUTPUT "${GENERATED_CPP_FILE}"
+        COMMAND ${Python3_EXECUTABLE} -u "${CODEGEN_SCRIPT}" ${CODEGEN_ARGS}
+        DEPENDS "${CODEGEN_SCRIPT}" "${CODEGEN_INPUTS}"
+        COMMENT "Generating ECS systems registry ${GENERATED_CPP_FILE}"
+        VERBATIM
 )
 
-set(GENERATED_DIR ${CMAKE_SOURCE_DIR}/generated)
-set(GENERATED_SRC ${GENERATED_DIR}/ECS_Systems.gen.cpp)
+# Define target and output parameters of script
+add_custom_target(ecs_codegen
+        DEPENDS "${GENERATED_CPP_FILE}"
+)
 
-add_custom_target(ecs_codegen ALL
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${GENERATED_DIR}
-        COMMAND ${Python3_EXECUTABLE} -u ${CMAKE_SOURCE_DIR}/tools/system_file_generator.py
-        --roots ${ECS_SCAN_DIRS}
-        --out ${GENERATED_SRC}
-        BYPRODUCTS "${ECS_GENERATED_SRC}"
-        DEPENDS ${ECS_SCAN_FILES} ${CMAKE_SOURCE_DIR}/tools/system_file_generator.py
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-        COMMENT "Generating ECS system registry..."
-        VERBATIM)
+set(GENERATED_CPP "${GENERATED_CPP_FILE}")
+set(GENERATED_FILES_DIR "${GENERATED_DIR}")
+
